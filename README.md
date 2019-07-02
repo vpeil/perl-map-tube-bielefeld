a8e555838099e7d1debb6e8c854ecbd53cb0cc47
Vitali Peil <vitali.peil@uni-bielefeld.de>
2019-07-02 06:48:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Map::Tube::Bielefeld - Blah blah blah
+
+# SYNOPSIS
+
+    use Map::Tube::Bielefeld;
+
+# DESCRIPTION
+
+Map::Tube::Bielefeld is
+
+# AUTHOR
+
+Vitali Peil <vitali.peil@uni-bielefeld.de>
+
+# COPYRIGHT
+
+Copyright 2019- Vitali Peil
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Map/Tube/Bielefeld.pm
@@ -0,0 +1,39 @@
+package Map::Tube::Bielefeld;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Map::Tube::Bielefeld - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Map::Tube::Bielefeld;
+
+=head1 DESCRIPTION
+
+Map::Tube::Bielefeld is
+
+=head1 AUTHOR
+
+Vitali Peil E<lt>vitali.peil@uni-bielefeld.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Vitali Peil
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
